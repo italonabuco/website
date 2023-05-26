4459d5b2714165694041fc7c8739d44fa2663bb4
Italo Nabuco <italonabuco@hotmail.com>
2023-05-25 21:20:04 -0300

    installs TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-	return <div>Italo Nabuco - Website</div>;
+	return (
+		<div className="h-screen container mx-auto flex justify-center items-center">
+			<h1 className="text-2xl font-bold tracking-widest">
+				Italo Nabuco - Website
+			</h1>
+		</div>
+	);
 }
 
 export default App;
